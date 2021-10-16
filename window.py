0303b9c7881a7c97003f03b1b2ce18ce6c1f1c56
poo3fuma <coolishpoo3@gmail.com>
2021-10-16 16:23:39 +0900

    メインウィンドウの作成

--- window.py
+++ window.py
@@ -2,6 +2,10 @@ import tkinter
 
 # ウィンドウの作成
 root = tkinter.Tk()
+root.title("めもっちょう")
+root.iconbitmap("notepad_icon.ico")
+root.geometry('500x400')
+root.config(bg='blue')
 
 # ウィンドウのループ処理
 root.mainloop()
