17079475da6d3cd716fefd37da6a9ee7d30d9572
poo3fuma <coolishpoo3@gmail.com>
2021-10-16 16:41:12 +0900

    サブウィンドウをひょうじできるようにしました

--- window.py
+++ window.py
@@ -7,5 +7,13 @@ root.iconbitmap("notepad_icon.ico")
 root.geometry('500x400')
 root.config(bg='blue')
 
+# サブウィンドウの作成
+sub_window = tkinter.Toplevel()
+sub_window.title('サブウィンドウDETH YO!')
+sub_window.iconbitmap('notepad_icon.ico')
+sub_window.geometry('300x300+500+500')
+sub_window.config(bg='red')
+
+
 # ウィンドウのループ処理
 root.mainloop()
